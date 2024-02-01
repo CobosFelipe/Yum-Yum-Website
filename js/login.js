1189f5c32b4d660f8b5012189768b5813803d8ed
CobosFelipe <flipper7247@gmail.com>
2024-02-01 17:01:33 -0500

    testing login logic

--- js/login.js
+++ js/login.js
@@ -1,6 +1,24 @@
-var myModal = document.getElementById('facebook-modal')
-var myInput = document.getElementById('myInput')
+(function () {
+  "use strict";
 
-myModal.addEventListener('shown.bs.modal', function () {
-  myInput.focus()
-})
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll(".needs-validation");
+
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms).forEach(function (form) {
+    form.addEventListener(
+      "submit",
+      function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        } else {
+          console.log("no entra");
+        }
+
+        form.classList.add("was-validated");
+      },
+      false
+    );
+  });
+})();
