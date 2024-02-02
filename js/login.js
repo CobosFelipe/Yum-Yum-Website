6b70421c0762bae6ffe30f8842dc726bebd29deb
CobosFelipe <flipper7247@gmail.com>
2024-02-02 08:00:33 -0500

    Animating forms and add styles

--- js/login.js
+++ js/login.js
@@ -22,3 +22,55 @@
     );
   });
 })();
+
+const cardRegistro = document.querySelector(".card-registro");
+const cardLogin = document.querySelector(".card-login");
+const btnRegistro = document.querySelector("#btn-registro");
+const btnInicio = document.querySelector("#btn-login");
+
+function framesRegistro() {
+  const animacion = cardRegistro.animate([
+    //KeyFrames
+    {transform: "rotateY(90deg)"},
+    {transform: "rotateY(180deg)"},
+    {display: "none"}
+  ],{
+    //Options
+    easing: "linear",
+    iterations: 1,
+    duration: 300 //miliseconds
+  });
+
+  return animacion.finished;
+}
+
+function framesLogin() {
+  const animacion = cardLogin.animate([
+    //KeyFrames
+    {transform: "rotateY(90deg)"},
+    {transform: "rotateY(180deg)"},
+    {display: "none"}
+  ],{
+    //Options
+    easing: "linear",
+    iterations: 1,
+    duration: 300 //miliseconds
+  });
+
+  return animacion.finished;
+}
+
+btnInicio.onclick = function(){
+  framesRegistro()
+}
+
+btnRegistro.onclick = function(){
+  framesLogin()
+}
+// function displace() {
+//   frames()
+//   .then((res)=>{
+//     console.log(res);
+
+//   })
+// }
