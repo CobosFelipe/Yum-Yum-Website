92bb45492a3eae37089b7c0bcf04d70ba74a1b8c
XimenaLargo96 <ximena0301@gmail.com>
2024-02-05 09:37:31 -0500

    Task 9 add validation form

--- js/login.js
+++ js/login.js
@@ -1,12 +1,25 @@
-const btnChangeForm = document.getElementById("bt-changeForm");
-const formLoginChange = document.getElementById("login-form");
-const registerFormChange = document.getElementById("register-form")
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+  "use strict";
 
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll(".needs-validation");
 
-btnChangeForm.addEventListener('click', (e) => {
-  // Agregar un pequeño retraso para permitir que las clases se eliminen antes de aplicar la transición
-  setTimeout(() => {
-    formLoginChange.classList.toggle("d-none");
-    registerFormChange.classList.toggle("d-none");
-  }, 0);
-});
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms).forEach(function (form) {
+    form.addEventListener(
+      "submit",
+      function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        } else {
+          console.log("no entra");
+        }
+
+        form.classList.add("was-validated");
+      },
+      false
+    );
+  });
+})();
