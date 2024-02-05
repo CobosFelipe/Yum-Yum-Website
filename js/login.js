12826a2c6825bfb3d102482faf5a46af4c4d9037
CobosFelipe <flipper7247@gmail.com>
2024-02-05 09:28:30 -0500

    general changes

--- js/login.js
+++ js/login.js
@@ -29,44 +29,58 @@ const btnRegistro = document.querySelector("#btn-registro");
 const btnInicio = document.querySelector("#btn-login");
 
 function framesRegistro() {
-  const animacion = cardRegistro.animate([
-    //KeyFrames
-    {transform: "rotateY(90deg)"},
-    {transform: "rotateY(180deg)"},
-    {display: "none"}
-  ],{
-    //Options
-    easing: "linear",
-    iterations: 1,
-    duration: 300 //miliseconds
-  });
+  const animacion = cardRegistro.animate(
+    [
+      //KeyFrames
+      { transform: "rotateY(90deg)" },
+      { transform: "rotateY(180deg)" },
+    ],
+    {
+      //Options
+      easing: "linear",
+      iterations: 1,
+      duration: 300, //miliseconds
+    }
+  );
+
+  setTimeout(() => {
+    cardLogin.classList.remove("d-none");
+    cardRegistro.classList.add("d-none");
+  }, "250");
 
   return animacion.finished;
 }
 
 function framesLogin() {
-  const animacion = cardLogin.animate([
-    //KeyFrames
-    {transform: "rotateY(90deg)"},
-    {transform: "rotateY(180deg)"},
-    {display: "none"}
-  ],{
-    //Options
-    easing: "linear",
-    iterations: 1,
-    duration: 300 //miliseconds
-  });
+  const animacion = cardLogin.animate(
+    [
+      //KeyFrames
+      { transform: "rotateY(90deg)" },
+      { transform: "rotateY(180deg)" },
+    ],
+    {
+      //Options
+      easing: "linear",
+      iterations: 1,
+      duration: 300, //miliseconds
+    }
+  );
+
+  setTimeout(() => {
+    cardLogin.classList.add("d-none");
+    cardRegistro.classList.remove("d-none");
+  }, "300");
 
   return animacion.finished;
 }
 
-btnInicio.onclick = function(){
-  framesRegistro()
-}
+btnInicio.onclick = function () {
+  framesRegistro();
+};
 
-btnRegistro.onclick = function(){
-  framesLogin()
-}
+btnRegistro.onclick = function () {
+  framesLogin();
+};
 // function displace() {
 //   frames()
 //   .then((res)=>{
